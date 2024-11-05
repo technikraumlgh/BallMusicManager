1368de732430ad9f6e78c475d1e33a6bfd592e6e
Barion <ametrin.studios@gmail.com>
2024-11-05 20:11:19 +0100

    addded publish.ps1

---
+++ publish.ps1
@@ -0,0 +1,3 @@
+dotnet publish .\BallMusicManager.Creator -o 'I:\Coding\TestChamber\BallMusic\Creator' -p:DebugType=None
+dotnet publish .\BallMusicManager.Player -o 'I:\Coding\TestChamber\BallMusic\Player' -p:DebugType=None
+dotnet publish .\BallMusicManager.Server -o 'I:\Coding\TestChamber\BallMusic\Server' -p:DebugType=None
