368da72754b159d24e40cf306072d679fa285da1
Barion <ametrin.studios@gmail.com>
2025-06-29 14:34:43 +0200

    added Slowfox and Quickstep

--- BallMusic.Domain/Dance.cs
+++ BallMusic.Domain/Dance.cs
@@ -10,20 +10,21 @@ public static class Dance
     public const string RockNRoll = "Rock 'n' Roll";
     public const string Party = "Party";
     public const string Pasodoble = "Pasodoble";
+    public const string Quickstep = "Quickstep";
     public const string Rumba = "Rumba";
     public const string Salsa = "Salsa";
     public const string Samba = "Samba";
+    public const string Slowfox = "Slowfox";
     public const string Tango = "Tango";
     public const string WienerWalzer = "Wiener Walzer";
 
     // this exists for compat with the old naming convention and to make entering dances easier
-    public static readonly FrozenDictionary<string, string> DanceSlugs = new Dictionary<string, string> {
+    public static readonly FrozenDictionary<string, string> DanceSlugs = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase) {
         {"CCC", ChaChaCha },
         {"LW", LangsamerWalzer },
         {"JVE", Jive },
         {"DFX", Discofox },
         {"FXT", Foxtrott },
-        {"FXTR", Foxtrott }, // legacy
         {"RMB", Rumba },
         {"WW", WienerWalzer },
         {"TGO", Tango },
@@ -33,8 +34,10 @@ public static class Dance
         {"PT", Party },
         {"FS", Party }, // legacy, was called Freestyle
         {"PSDB", Pasodoble },
+        {"SFX", Slowfox },
+        {"QS", Quickstep },
     }.ToFrozenDictionary();
 
     // when ever a dance string enters the program it checks whether it is a known slug and if so replaces it
-    public static string FromSlug(string key) => DanceSlugs.TryGetValue(key.ToUpper(), out var name) ? name : key;
+    public static string FromSlug(string key) => DanceSlugs.TryGetValue(key, out var name) ? name : key;
 }
